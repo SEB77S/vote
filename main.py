23b5a6696ccc44273cabba834e485fba5dd55409
SEB77S <107449059+SEB77S@users.noreply.github.com>
2025-05-22 19:08:00 -0500

    Commit Inicial

---
+++ main.py
@@ -0,0 +1,7 @@
+# mi_proyecto/main.py
+
+def main():
+    print("Hola, mundo desde mi proyecto base.")
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_main.py
@@ -0,0 +1,4 @@
+# tests/test_main.py
+
+def test_ejemplo():
+    assert 1 + 1 == 2
